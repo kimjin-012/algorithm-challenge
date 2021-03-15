004180c5ff256300443e9694bd8ac7dad00e542e
Jin Kim <77659317+kimjin-012@users.noreply.github.com>
2021-03-15 11:56:05 -0700

    Update march_15-array_duplicate_check.js

--- JavaScript practice/march_15-array_duplicate_check.js
+++ JavaScript practice/march_15-array_duplicate_check.js
@@ -33,34 +33,4 @@ function dedupeSorted(nums) {
 
 console.log(dedupeSorted(nums3))
 
-/*****************************************************************************/
-
-/* 
-Array: Mode
-
-Create a function that, given an array of ints,
-returns the int that occurs most frequently in the array.
-What if there are multiple items that occur the same number of time?
-    - return all of them (in an array)
-    - what if all items occur the same number of times?
-    - return empty array
-*/
-
-const nums1 = [];
-const expected1 = [];
-
-const nums2 = [1];
-const expected2 = [1];
-
-const nums3 = [5, 1, 4];
-const expected3 = [];
-
-const nums4 = [5, 1, 4, 1];
-const expected4 = [1];
-
-const nums5 = [5, 1, 4, 1, 5];
-const expected5 = [5, 1];
-//  - order doesn't matter
-
-function mode(nums) {}
 
