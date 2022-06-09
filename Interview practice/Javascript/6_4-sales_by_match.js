1fae765b23d92bebe487216e936d748be6081d0b
Jin Kim <na02093@gmail.com>
2022-06-08 23:26:25 -0700

    Organizing files

--- Interview practice/Javascript/6_4-sales_by_match.js
+++ Interview practice/Javascript/6_4-sales_by_match.js
@@ -5,7 +5,7 @@ const a = 9;
 const ar = [10,20,20,10,10,30,50,10,20];
 
 function sockMerchant(n, ar) {
-    // Write your code here
+    // Write your code here`
     var object = {};
     var pair = 0;
     for(var i=0; i<n; i++)
